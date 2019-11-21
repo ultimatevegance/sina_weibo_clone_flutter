0a03e12d5c8a284d112a95fb11f27eb58885e16d
justus <martinvegance@yahoo.com>
2019-11-21 11:06:34 +0800

    fix tabbar icons

--- sina_weibo_flutter/lib/main.dart
+++ sina_weibo_flutter/lib/main.dart
@@ -24,14 +24,29 @@ class IndexTab extends StatelessWidget {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         items: [
-          BottomNavigationBarItem(icon:  new Image.asset("assets/images/tabbar_home@3x.png",),activeIcon: new Image.asset("assets/images/tabbar_home_selected@3x.png") ,title: Text('微博')),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.video_camera),title: Text('视频')),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.search),title: Text('发现')),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.mail),title: Text('消息')),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person),title: Text('我')),
+          BottomNavigationBarItem(
+              icon:  new Image.asset("assets/images/tabbar_home@3x.png",width:30,height: 30 ,),
+              activeIcon: new Image.asset("assets/images/tabbar_home_selected@3x.png",width: 30,height: 30,) ,
+              title: Text('微博')),
+          BottomNavigationBarItem(
+              icon: new Image.asset("assets/images/tabbar_video@3x.png",width: 30,height: 30,),
+              activeIcon: new Image.asset("assets/images/tabbar_video_selected@3x.png",width: 30,height: 30,),
+              title: Text('视频')),
+          BottomNavigationBarItem(
+              icon: new Image.asset("assets/images/tabbar_discover@3x.png",width: 30,height: 30,),
+              activeIcon: new Image.asset("assets/images/tabbar_discover_selected@3x.png",width: 30,height: 30,),
+              title: Text('发现')),
+          BottomNavigationBarItem(
+              icon: new Image.asset("assets/images/tabbar_message_center@3x.png",width: 30,height: 30,),
+              activeIcon:  new Image.asset("assets/images/tabbar_message_center_selected@3x.png",width: 30,height: 30,),
+              title: Text('消息')),
+          BottomNavigationBarItem(
+              icon: new Image.asset("assets/images/tabbar_profile@3x.png",width: 30,height: 30,),
+              activeIcon: new Image.asset("assets/images/tabbar_profile_selected@3x.png",width: 30,height: 30,),
+              title: Text('我')),
         ],
-        activeColor: Colors.redAccent,
-        inactiveColor: Colors.green,
+        activeColor: Colors.black,
+        inactiveColor: Colors.black,
       ),
       // ignore: missing_return
       tabBuilder: (context,index){
