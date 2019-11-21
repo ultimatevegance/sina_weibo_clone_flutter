5880b3d5546e1f08ff29eebc28a25bfe9214d516
justus <martinvegance@yahoo.com>
2019-11-21 11:07:49 +0800

    Update main.dart

--- sina_weibo_flutter/lib/main.dart
+++ sina_weibo_flutter/lib/main.dart
@@ -25,56 +25,104 @@ class IndexTab extends StatelessWidget {
       tabBar: CupertinoTabBar(
         items: [
           BottomNavigationBarItem(
-              icon:  new Image.asset("assets/images/tabbar_home@3x.png",width:30,height: 30 ,),
-              activeIcon: new Image.asset("assets/images/tabbar_home_selected@3x.png",width: 30,height: 30,) ,
+              icon: new Image.asset(
+                "assets/images/tabbar_home@3x.png",
+                width: 30,
+                height: 30,
+              ),
+              activeIcon: new Image.asset(
+                "assets/images/tabbar_home_selected@3x.png",
+                width: 30,
+                height: 30,
+              ),
               title: Text('微博')),
           BottomNavigationBarItem(
-              icon: new Image.asset("assets/images/tabbar_video@3x.png",width: 30,height: 30,),
-              activeIcon: new Image.asset("assets/images/tabbar_video_selected@3x.png",width: 30,height: 30,),
+              icon: new Image.asset(
+                "assets/images/tabbar_video@3x.png",
+                width: 30,
+                height: 30,
+              ),
+              activeIcon: new Image.asset(
+                "assets/images/tabbar_video_selected@3x.png",
+                width: 30,
+                height: 30,
+              ),
               title: Text('视频')),
           BottomNavigationBarItem(
-              icon: new Image.asset("assets/images/tabbar_discover@3x.png",width: 30,height: 30,),
-              activeIcon: new Image.asset("assets/images/tabbar_discover_selected@3x.png",width: 30,height: 30,),
+              icon: new Image.asset(
+                "assets/images/tabbar_discover@3x.png",
+                width: 30,
+                height: 30,
+              ),
+              activeIcon: new Image.asset(
+                "assets/images/tabbar_discover_selected@3x.png",
+                width: 30,
+                height: 30,
+              ),
               title: Text('发现')),
           BottomNavigationBarItem(
-              icon: new Image.asset("assets/images/tabbar_message_center@3x.png",width: 30,height: 30,),
-              activeIcon:  new Image.asset("assets/images/tabbar_message_center_selected@3x.png",width: 30,height: 30,),
+              icon: new Image.asset(
+                "assets/images/tabbar_message_center@3x.png",
+                width: 30,
+                height: 30,
+              ),
+              activeIcon: new Image.asset(
+                "assets/images/tabbar_message_center_selected@3x.png",
+                width: 30,
+                height: 30,
+              ),
               title: Text('消息')),
           BottomNavigationBarItem(
-              icon: new Image.asset("assets/images/tabbar_profile@3x.png",width: 30,height: 30,),
-              activeIcon: new Image.asset("assets/images/tabbar_profile_selected@3x.png",width: 30,height: 30,),
+              icon: new Image.asset(
+                "assets/images/tabbar_profile@3x.png",
+                width: 30,
+                height: 30,
+              ),
+              activeIcon: new Image.asset(
+                "assets/images/tabbar_profile_selected@3x.png",
+                width: 30,
+                height: 30,
+              ),
               title: Text('我')),
         ],
         activeColor: Colors.black,
         inactiveColor: Colors.black,
       ),
       // ignore: missing_return
-      tabBuilder: (context,index){
-        switch(index) {
+      tabBuilder: (context, index) {
+        switch (index) {
           case 0:
-          return CupertinoTabView(builder: (context){
-            return HomePageVC();
-          },);
+            return CupertinoTabView(
+              builder: (context) {
+                return HomePageVC();
+              },
+            );
           case 1:
-            return CupertinoTabView(builder: (context){
-              return VideoPageVC();
-            },);
+            return CupertinoTabView(
+              builder: (context) {
+                return VideoPageVC();
+              },
+            );
           case 2:
-            return CupertinoTabView(builder: (context){
-              return DiscoverPageVC();
-            },);
+            return CupertinoTabView(
+              builder: (context) {
+                return DiscoverPageVC();
+              },
+            );
           case 3:
-            return CupertinoTabView(builder: (context){
-              return MessagePageVC();
-            },);
+            return CupertinoTabView(
+              builder: (context) {
+                return MessagePageVC();
+              },
+            );
           case 4:
-            return CupertinoTabView(builder: (context){
-              return MyPageVC();
-            },);
+            return CupertinoTabView(
+              builder: (context) {
+                return MyPageVC();
+              },
+            );
         }
       },
     );
   }
 }
-
-
