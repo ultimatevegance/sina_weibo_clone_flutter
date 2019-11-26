30a1bc2b2fcf691ff0e642e93716fcf2bd8452e9
justus <martinvegance@yahoo.com>
2019-11-26 14:54:53 +0800

    fix

--- sina_weibo_flutter/lib/pages/homePage.dart
+++ sina_weibo_flutter/lib/pages/homePage.dart
@@ -6,31 +6,48 @@ class HomePageVC extends StatefulWidget {
   _HomePageVCState createState() => _HomePageVCState();
 }
 
-class _HomePageVCState extends State<HomePageVC> {
+class _HomePageVCState extends State<HomePageVC> with SingleTickerProviderStateMixin{
+ var titleTabList = ["推荐","关注"];
+ List<Widget> tabLists;
+ TabController _tabController;
+ var tabbar;
+
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(vsync: this,length: titleTabList.length);
+    tabLists = getTabs();
+    tabbar =
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Center(
-        child: Text('微博'),
-      ),
-      navigationBar: CupertinoNavigationBar(
+    return Scaffold(
+      appBar:CupertinoNavigationBar(
         leading: CupertinoButton(
           padding: EdgeInsets.zero,
           child:  Image.asset("assets/images/navigationbar_icon_camera@3x.png",width: 30,height: 30,),
           onPressed: () {
           },
         ),
-        middle: TabBar(tabs: <Widget>[
-          new Tab(text: "关注",),
-          new Tab(text: "推荐",)
-        ],),
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
           child:  Image.asset("assets/images/navigationbar_icon_compose@3x.png",width: 30,height: 30,),
           onPressed: () {
           },
         ),
-      ) ,
+      )  ,
     );
   }
+
+ List<Widget> getTabs() {
+   return titleTabList.map(
+       (item) => Text(
+         '$item',style: TextStyle(fontSize: 15),
+       )
+   ).toList();
+ }
+
 }
---
+++ sina_weibo_flutter/lib/wigets/MTabbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MTabbar extends StatefulWidget {
+  @override
+  _MTabbarState createState() => _MTabbarState();
+}
+
+class _MTabbarState extends State<MTabbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
